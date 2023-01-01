ab55baf7b608ba2ae7fb0aff3b9c30ab50863713
Md. Murad Hossin <48625061+muradhossin@users.noreply.github.com>
2023-01-01 18:59:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -16,18 +16,45 @@ ExerciseAnginaList = ["Select ExerciseAngina", 0,1]
 STSlopeList = ['Select ST_Slope', 0, 1,2]
 FastingBSList = ['Select FastingBS', 0, 1,]
 
+col1, col2 = st.columns(2)
 
-Age = st.slider('Age', min_value= 1, max_value= 110, step=1, value= 20)
-Sex = st.selectbox("Sex [ 0 = Male, 1 = Female ]", genderList)
-ChestPainType = st.selectbox("ChestPainType [ 0 = Asymptomatic, 1 = Non-Anginal Pain, 2 = Atypical Angina, 3 = Typical Angina, ]", ChestPainTypeList)
-RestingBP = st.slider("RestingBP ", min_value= 0, max_value= 200, step=1, value= 100)
-Cholesterol = st.slider("Cholesterol ", min_value= 0, max_value= 603, step=1, value= 200)
-FastingBS = st.selectbox('FastingBS [ 0 = otherwise, 1 = if FastingBS > 120 mg/dl ]', FastingBSList)
-RestingECG = st.selectbox('RestingECG [ 0 = Normal, 1 = showing probable, 2 = abnormality ,]', RestingECGList)
-MaxHR = st.slider('MaxHR', min_value= 60, max_value= 202, step=1, value= 100)
-ExerciseAngina = st.selectbox("ExerciseAngina [ 0 = No, 1 = Yes ]", ExerciseAnginaList)
-Oldpeak = st.slider("Oldpeak", min_value= -2.6, max_value= 6.2, step=0.01, value=0.0)
-ST_Slope = st.selectbox('ST_Slope [ 0 = flat, 1 = upsloping, 2 = downsloping,]', STSlopeList)
+with col1:
+    Age = st.slider('Age', min_value= 1, max_value= 110, step=1, value= 20)
+with col2:
+    Sex = st.selectbox("Sex [ 0 = Male, 1 = Female ]", genderList)
+with col1:
+    ChestPainType = st.selectbox("ChestPainType [ 0 = Asymptomatic, 1 = Non-Anginal Pain, 2 = Atypical Angina, 3 = Typical Angina, ]", ChestPainTypeList)
+with col2:   
+    RestingBP = st.slider("RestingBP ", min_value= 0, max_value= 200, step=1, value= 100)
+with col1:
+    Cholesterol = st.slider("Cholesterol ", min_value= 0, max_value= 603, step=1, value= 200)
+with col2:
+    FastingBS = st.selectbox('FastingBS [ 0 = otherwise, 1 = if FastingBS > 120 mg/dl ]', FastingBSList)
+with col1:
+    RestingECG = st.selectbox('RestingECG [ 0 = Normal, 1 = showing probable, 2 = abnormality ,]', RestingECGList)
+with col2:
+    MaxHR = st.slider('MaxHR', min_value= 60, max_value= 202, step=1, value= 100)
+with col1:
+    ExerciseAngina = st.selectbox("ExerciseAngina [ 0 = No, 1 = Yes ]", ExerciseAnginaList)
+with col2:
+    Oldpeak = st.slider("Oldpeak", min_value= -2.6, max_value= 6.2, step=0.01, value=0.0)
+with col2:
+    ST_Slope = st.selectbox('ST_Slope [ 0 = flat, 1 = upsloping, 2 = downsloping,]', STSlopeList)
+
+
+
+
+# Age = st.slider('Age', min_value= 1, max_value= 110, step=1, value= 20)
+# Sex = st.selectbox("Sex [ 0 = Male, 1 = Female ]", genderList)
+# ChestPainType = st.selectbox("ChestPainType [ 0 = Asymptomatic, 1 = Non-Anginal Pain, 2 = Atypical Angina, 3 = Typical Angina, ]", ChestPainTypeList)
+# RestingBP = st.slider("RestingBP ", min_value= 0, max_value= 200, step=1, value= 100)
+# Cholesterol = st.slider("Cholesterol ", min_value= 0, max_value= 603, step=1, value= 200)
+# FastingBS = st.selectbox('FastingBS [ 0 = otherwise, 1 = if FastingBS > 120 mg/dl ]', FastingBSList)
+# RestingECG = st.selectbox('RestingECG [ 0 = Normal, 1 = showing probable, 2 = abnormality ,]', RestingECGList)
+# MaxHR = st.slider('MaxHR', min_value= 60, max_value= 202, step=1, value= 100)
+# ExerciseAngina = st.selectbox("ExerciseAngina [ 0 = No, 1 = Yes ]", ExerciseAnginaList)
+# Oldpeak = st.slider("Oldpeak", min_value= -2.6, max_value= 6.2, step=0.01, value=0.0)
+# ST_Slope = st.selectbox('ST_Slope [ 0 = flat, 1 = upsloping, 2 = downsloping,]', STSlopeList)
 
 
 #code for prediction
